9c00a3f612d9f3525275582c730d9191fcc732cb
Md Kamrul Hasan <mdkamrulhasanewu@gmail.com>
2021-06-03 19:13:13 +0600

    react

---
+++ src/iplfrontend/src/components/MatchDetailCard.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+
+export const MatchDetailCard = () => {
+    return (
+      <div className="MatchDetailCard">
+        <h1>Team Name</h1>
+        <h3>Latest Matches</h3>
+      </div>
+    );
+  }
+  
+  
