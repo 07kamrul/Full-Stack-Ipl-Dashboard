b8b0e16389381d8c7f07c8b4ed26ca4006db7209
Md Kamrul Hasan <mdkamrulhasanewu@gmail.com>
2021-06-05 10:21:59 +0600

    get data from db

--- src/iplfrontend/src/components/MatchDetailCard.js
+++ src/iplfrontend/src/components/MatchDetailCard.js
@@ -1,10 +1,13 @@
 import { React } from 'react';
 
-export const MatchDetailCard = () => {
-    return (
+export const MatchDetailCard = ({match}) => {
+  if(!match) return  null;
+  
+  return (
       <div className="MatchDetailCard">
         <h1>Team Name</h1>
         <h3>Latest Matches</h3>
+        <h4>{match.team1} vs {match.team2}</h4>
       </div>
     );
   }
